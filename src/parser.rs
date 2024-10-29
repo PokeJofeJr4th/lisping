ead52d519ddbeceac1788dbe3a283d31e558b6e0
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-10-29 19:00:42 -0400

    better parser implementation

--- src/parser.rs
+++ src/parser.rs
@@ -8,7 +8,7 @@ enum ParserState {
     Unquote,
 }
 
-#[allow(clippy::unnecessary_wraps)]
+#[allow(clippy::too_many_lines)]
 /// # Errors
 pub fn parse(src: &str) -> Result<Vec<Value>, String> {
     let mut chars = src.chars().line_count().peekable();
@@ -17,102 +17,89 @@ pub fn parse(src: &str) -> Result<Vec<Value>, String> {
     let mut states: Vec<ParserState> = Vec::new();
     // the array of atoms at the current depth
     let mut current_array: Vec<Value> = Vec::new();
-    'main: while let Some((row, col, c)) = chars.next() {
-        // println!("{parse_stack:#?}\n{current_array:#?}\n{row}:{col} = {c:?}");
-        // begin a comment
-        if c == '#' {
-            // go to the end of the line
-            for (_, _, c) in chars.by_ref() {
-                if c == '\n' {
-                    continue 'main;
-                }
-            }
-        } else if c == '(' {
-            // begin a new array
-            parse_stack.push(current_array);
-            states.push(ParserState::Array);
-            current_array = Vec::new();
-        } else if c == ')' {
-            // end the current array
-            if let Some(previous_level) = parse_stack.pop() {
-                let arr = Value::Array(current_array);
-                current_array = previous_level;
-                states.pop();
-                if states.last() == Some(&ParserState::Quote) {
-                    current_array.push(Value::Array(vec![
-                        Value::Identifier("quote".to_string()),
-                        arr,
-                    ]));
-
-                    states.pop();
-                } else if states.last() == Some(&ParserState::QuasiQuote) {
-                    current_array.push(Value::Array(vec![
-                        Value::Identifier("quasiquote".to_string()),
-                        arr,
-                    ]));
-                    states.pop();
-                } else if states.last() == Some(&ParserState::Unquote) {
-                    current_array.push(Value::Array(vec![
-                        Value::Identifier("unquote".to_string()),
-                        arr,
-                    ]));
-                    states.pop();
+    'main: loop {
+        let next_thing = 'inner: {
+            'by_char: while let Some((row, col, c)) = chars.next() {
+                // println!("{parse_stack:#?}\n{current_array:#?}\n{row}:{col} = {c:?}");
+                // begin a comment
+                if c == '#' {
+                    // go to the end of the line
+                    for (_, _, c) in chars.by_ref() {
+                        if c == '\n' {
+                            continue 'by_char;
+                        }
+                    }
+                } else if c == '(' {
+                    // begin a new array
+                    parse_stack.push(current_array);
+                    states.push(ParserState::Array);
+                    current_array = Vec::new();
+                } else if c == ')' {
+                    // end the current array
+                    if let (Some(previous_level), Some(ParserState::Array)) =
+                        (parse_stack.pop(), states.pop())
+                    {
+                        let arr = Value::Array(current_array);
+                        current_array = previous_level;
+                        break 'inner arr;
+                    }
+                    return Err(format!("Unmatched closing parenthesis at {row}:{col}"));
+                } else if c == '\'' {
+                    states.push(ParserState::Quote);
+                } else if c == '`' {
+                    states.push(ParserState::QuasiQuote);
+                } else if c == '~' {
+                    states.push(ParserState::Unquote);
+                } else if c.is_whitespace() {
+                } else if c.is_numeric() {
+                    let mut int_value = c as u32 - '0' as u32;
+                    while let Some((row, col, c)) = chars.peek() {
+                        if c.is_whitespace() || *c == '(' || *c == ')' || *c == '#' || *c == '\'' {
+                            break;
+                        }
+                        if !c.is_numeric() {
+                            return Err(format!(
+                                "Invalid character in int literal: `{c:?}`; {row}:{col}"
+                            ));
+                        }
+                        int_value *= 10;
+                        int_value += *c as u32 - '0' as u32;
+                        chars.next();
+                    }
+                    break 'inner Value::Int(int_value as i32);
                 } else {
-                    current_array.push(arr);
+                    let mut id_buffer = String::from(c);
+                    while let Some((_, _, c)) = chars.peek() {
+                        if c.is_whitespace() || *c == '(' || *c == ')' || *c == '#' {
+                            break;
+                        }
+                        id_buffer.push(*c);
+                        chars.next();
+                    }
+                    break 'inner Value::Identifier(id_buffer);
                 }
-            } else {
-                return Err(format!("Unmatched closing parenthesis at {row}:{col}"));
             }
-        } else if c == '\'' {
-            states.push(ParserState::Quote);
-        } else if c == '`' {
-            states.push(ParserState::QuasiQuote);
-        } else if c == '~' {
-            states.push(ParserState::Unquote);
-        } else if c.is_whitespace() {
-        } else if c.is_numeric() {
-            let mut int_value = c as u32 - '0' as u32;
-            while let Some((row, col, c)) = chars.peek() {
-                if c.is_whitespace() || *c == '(' || *c == ')' || *c == '#' || *c == '\'' {
-                    break;
-                }
-                if !c.is_numeric() {
-                    return Err(format!(
-                        "Invalid character in int literal: `{c:?}`; {row}:{col}"
-                    ));
-                }
-                int_value *= 10;
-                int_value += *c as u32 - '0' as u32;
-                chars.next();
-            }
-            if states.last() == Some(&ParserState::Quote) {
-                current_array.push(Value::Array(vec![
-                    Value::Identifier("quote".to_string()),
-                    (Value::Int(int_value as i32)),
-                ]));
+            break 'main;
+        };
+        let next_thing = match states.last() {
+            Some(ParserState::Quote) => {
                 states.pop();
-            } else {
-                current_array.push(Value::Int(int_value as i32));
+                Value::Array(vec![Value::Identifier("quote".to_string()), next_thing])
             }
-        } else {
-            let mut id_buffer = String::from(c);
-            while let Some((_, _, c)) = chars.peek() {
-                if c.is_whitespace() || *c == '(' || *c == ')' || *c == '#' {
-                    break;
-                }
-                id_buffer.push(*c);
-                chars.next();
-            }
-            if states.last() == Some(&ParserState::Quote) {
-                current_array.push(Value::Array(vec![
-                    Value::Identifier("quote".to_string()),
-                    Value::Identifier(id_buffer),
-                ]));
+            Some(ParserState::QuasiQuote) => {
                 states.pop();
-            } else {
-                current_array.push(Value::Identifier(id_buffer));
+                Value::Array(vec![
+                    Value::Identifier("quasiquote".to_string()),
+                    next_thing,
+                ])
             }
-        }
+            Some(ParserState::Unquote) => {
+                states.pop();
+                Value::Array(vec![Value::Identifier("unquote".to_string()), next_thing])
+            }
+            _ => next_thing,
+        };
+        current_array.push(next_thing);
     }
     if parse_stack.is_empty() {
         Ok(current_array)
